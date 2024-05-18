c644a2151a72c408cfcb6d9fc36dca54825534db
JuanCMPDev <juancarlosmpdev@gmail.com>
2024-05-17 22:49:51 -0500

    Fifth commit: add the client

---
+++ client/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8800/api/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    },
+  server: {
+    host: true
+  }
+  },
+  plugins: [react()],
+})
