9413b522f1fefb5d9d32ca66d4e0ba423122e745
Juan C Muñoz <juancarlosmpdev@gmail.com>
2024-05-18 10:14:31 -0500

    Sixth commit: deleted info for firebase

--- client/src/firebase/config.js
+++ client/src/firebase/config.js
@@ -2,14 +2,7 @@ import { initializeApp } from "firebase/app";
 import {getStorage, ref, uploadBytes, getDownloadURL} from 'firebase/storage'
 import {v4} from 'uuid'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDR6w-iTdME2EJ_kd_jEIVg-PYI9YwipBA",
-  authDomain: "react-blog-b29eb.firebaseapp.com",
-  projectId: "react-blog-b29eb",
-  storageBucket: "react-blog-b29eb.appspot.com",
-  messagingSenderId: "506307783824",
-  appId: "1:506307783824:web:dcfc74a86409728ef07c15"
-};
+// firebase config here:
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
