563ddd320300713faeb8e81342bb806a5534cc89
Juan C Muñoz <juancarlosmpdev@gmail.com>
2024-06-10 19:36:41 -0500

    final fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,17 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8800/api/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    },
-  server: {
     host: true
-  }
   },
   plugins: [react()],
-  base: 'https://JuanCMPDev.github.io/React-Blog',
 })
